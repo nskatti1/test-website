4445ae4c80fc7e5213165f61c74488e5659ff622
nskatti1 <137136436+nskatti1@users.noreply.github.com>
2025-02-04 21:13:05 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Fast Robots'
+project = 'Fast Robots ECE 4160'
 copyright = '2025, Nita Kattimani'
 author = 'Nita Kattimani'
 
